9bcde22f82f809233b56df59906e11f48b887e4e
Cemre61 <c.sari61@hotmail.com>
2015-01-27 12:31:27 +0100

    Revert "Revert "whackwhack""
    This reverts commit b088921c8db38985f8a6080e0b20c6f90d25a20a.

---
+++ Assets/RemoveWhackParticles.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveWhackParticles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy(this.gameObject,1f);
+	}
+}
